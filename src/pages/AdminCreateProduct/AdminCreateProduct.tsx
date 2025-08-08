fb6ed3191a43a8fcd76aeed4ea3194049de64aba
rivaldo-qq <70063901+rivaldo-qq@users.noreply.github.com>
2025-08-08 23:08:24 +0700

    Update AdminCreateProduct.tsx

--- src/pages/AdminCreateProduct/AdminCreateProduct.tsx
+++ src/pages/AdminCreateProduct/AdminCreateProduct.tsx
@@ -25,7 +25,7 @@ function AdminCreateProduct() {
             const formData = new FormData();
             formData.append("image", values.image[0]);
 
-            const uploadResponse = await axios.post<uploadImageResponse>("https://old-geckos-sleep.loca.lt/product/upload", formData);
+            const uploadResponse = await axios.post<uploadImageResponse>("https://slick-spoons-tickle.loca.lt/product/upload", formData);
             if (uploadResponse.status !== 200) {
                 Swal.fire({
                     title: "Upload Gambar Gagal",
