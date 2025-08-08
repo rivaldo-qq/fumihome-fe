828db6191c5be87d66117cb75343fd23e24cde07
rivaldo-qq <70063901+rivaldo-qq@users.noreply.github.com>
2025-08-08 20:22:56 +0700

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -33,43 +33,8 @@ function Footer() {
                     <img src="/images/sofa.png" alt="Sofa" className="img-fluid" />
                 </div>
 
-                <div className="row">
-                    <div className="col-lg-8">
-                        <div className="subscription-form">
-                            <h3 className="d-flex align-items-center">
-                                <span className="me-1">
-                                    <img src="/images/envelope-outline.svg" alt="Amplop" className="img-fluid" />
-                                </span>
-                                <span>Langganan Newsletter</span>
-                            </h3>
-
-                            <form className="row g-3" onSubmit={handleSubmit}>
-                                <div className="col-auto">
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="Masukkan nama Anda"
-                                        onChange={(e) => setFullName(e.target.value)}
-                                        value={fullName}
-                                    />
-                                </div>
-                                <div className="col-auto">
-                                    <input
-                                        type="email"
-                                        className="form-control"
-                                        placeholder="Masukkan email Anda"
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        value={email}
-                                    />
-                                </div>
-                                <div className="col-auto">
-                                    <button className="btn btn-primary">
-                                        <span className="fa fa-paper-plane"></span>
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+                
+                    
                 </div>
 
                 <div className="border-top copyright">
