48aa7d3f9d740deabbcd0b3db81b9dc2e2fc00a4
rivaldo-qq <70063901+rivaldo-qq@users.noreply.github.com>
2025-08-08 20:31:13 +0700

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,30 +1,4 @@
-import { useState } from "react";
-import useGrpcApi from "../../hooks/useGrpcApi";
-import { getNewsletterClient } from "../../api/grpc/client";
-import Swal from "sweetalert2";
-
 function Footer() {
-    const subscribeApi = useGrpcApi();
-    const currentYear = new Date().getFullYear();
-    const [fullName, setFullName] = useState<string>('');
-    const [email, setEmail] = useState<string>('');
-
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-
-        await subscribeApi.callApi(getNewsletterClient().subscribeNewsletter({
-            email: email,
-            fullName: fullName,
-        }));
-
-        await Swal.fire({
-            icon: 'success',
-            title: 'Berhasil Subscribe Newsletter'
-        });
-
-        setFullName('');
-        setEmail('');
-    };
 
     return (
         <footer className="footer-section">
@@ -32,8 +6,6 @@ function Footer() {
                 <div className="sofa-img">
                     <img src="/images/sofa.png" alt="Sofa" className="img-fluid" />
                 </div>
-
-                
             </div>
         </footer>
     )
