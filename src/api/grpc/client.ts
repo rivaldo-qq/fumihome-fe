901763013af03acd54d8f31e58f96dae1d5cee37
rivaldo-qq <70063901+rivaldo-qq@users.noreply.github.com>
2025-08-08 13:35:44 +0700

    Update client.ts

--- src/api/grpc/client.ts
+++ src/api/grpc/client.ts
@@ -16,7 +16,7 @@ let newsletterClient: INewsletterServiceClient | null = null;
 const getWebTransport = () => {
     if (webTransport === null) {
         webTransport = new GrpcWebFetchTransport({
-            baseUrl: "https://6a10091ba577.ngrok-free.app",
+            baseUrl: "https://e09bbbfdd8b8.ngrok-free.app",
             interceptors: [authInterceptor],
         })
     }
